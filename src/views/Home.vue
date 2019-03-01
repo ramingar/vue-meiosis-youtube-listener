88a7408cff8ccebcdf8301fb633ddf6f8b45f66e
Rafael Minguet <rminguet@gmail.com>
2019-03-01 20:57:23 +0100

    Clean project removing example pages from vue-cli

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <search/>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
+    import Search from '@/components/Search.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name      : 'home',
+        components: {
+            Search
+        }
+    }
 </script>
