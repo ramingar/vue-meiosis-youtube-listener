338213d528d0411ffac0d4ec3e658c1c9655407f
Rafael Minguet <rminguet@gmail.com>
2019-03-03 01:32:19 +0100

    Search videos only

--- src/config/config.dist.js
+++ src/config/config.dist.js
@@ -1,13 +1,14 @@
 const apiKey            = '~';
 const maxResults        = 50;
 const part              = 'snippet';
+const type              = 'video';
 const apiYoutubebaseUrl = 'https://www.googleapis.com/youtube/v3/search';
 
 export default {
     api: {
         youtube: {
             apiKey,
-            baseUrl: `${apiYoutubebaseUrl}?key=${apiKey}&part=${part}&maxResults=${maxResults}&q=`
+            baseUrl: `${apiYoutubebaseUrl}?key=${apiKey}&part=${part}&type=${type}&maxResults=${maxResults}&q=`
         }
     }
 }
